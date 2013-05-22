3f7df644f12b27c7a952b741f183c1cda901d7a7
cleversoap <clever@cleversoap.com>
2013-05-23 09:25:15 +1000

    Tabs to spaces, ignoring tags file

--- lib/lump.rb
+++ lib/lump.rb
@@ -1,25 +1,25 @@
 require 'thor'
 
 class Lump < Thor
+    
+    autoload :Actions, 'lump/actions'
 
-	autoload :Actions, 'lump/actions'
+    include Thor::Actions
+    include Lump::Actions
 
-	include Thor::Actions
-	include Lump::Actions
+    desc "new NAME", "Create a new lump template"
+    def new(name)
+        puts "Creating new template: " + name
+    end
 
-	desc "new NAME", "Create a new lump template"
-	def new(name)
-		puts "Creating new template: " + name
-	end
+    desc "make NAME", "Build a new project from the given template"
+    def make(name, path='.')
+        puts "Building template from: " + name
+        puts "Building it in path: " + path
+    end
 
-	desc "make NAME", "Build a new project from the given template"
-	def make(name, path='.')
-		puts "Building template from: " + name
-		puts "Building it in path: " + path
-	end
-
-	desc "list", "Lists all available lumps"
-	def list
-	end
+    desc "list", "Lists all available lumps"
+    def list
+    end
 
 end
--- lib/lump/actions.rb
+++ lib/lump/actions.rb
@@ -1,27 +1,27 @@
 class Lump
 
-	module Actions
+    module Actions
 
-		# Begins loading a lump project directory
-		def load(name)
+        # Begins loading a lump project directory
+        def load(name)
 
-			# Check if the directory exists
+            # Check if the directory exists
 
-			# If no name is supplied check one of the default configurations
-			# otherwise go to the named subdirectory
-			
-			# Check if a project file can be found
-			
-			# Load the project file
+            # If no name is supplied check one of the default configurations
+            # otherwise go to the named subdirectory
 
-			# Return the project object and project files directory
+            # Check if a project file can be found
 
-		end
+            # Load the project file
 
-		# Processes the project object
-		def process_project(proj)
-		end
-	
-	end
+            # Return the project object and project files directory
+
+        end
+
+        # Processes the project object
+        def process_project(proj)
+        end
+
+    end
 
 end
